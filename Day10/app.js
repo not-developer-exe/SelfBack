841d789ccb7746d8fb6c5a02237e2133ddad6af9
not-developer-exe <official.additya.singh@gmail.com>
2025-02-12 14:12:23 +0530

    Day 11 continued

--- Day10/app.js
+++ Day10/app.js
@@ -2,20 +2,33 @@ const express = require('express')
 const app = express()
 const userModel = require('./models/userModel')
 
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 app.get('/', (req,res)=>{
     res.send('Hey')
 })
 
-app.post('./create',async (req, res)=>{
+app.post('/create',async (req, res)=>{
     let user = await userModel.create({
-        name: req.params.name,
-        email: req.params.email,
-        pasword: req.params.password
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
     })
 
+    console.log(user)
     res.send(user)
 })
 
-app.listen(3000, ()=>{
+app.post('/:name/post', async (req,res)=>{
+    let user = await userModel.findOne({name: req.params.name})
+    user.post.push({content: 'Ye gyi post teeeesraaaaa...'})
+    await user.save()
+    res.send(user)
+})
+
+
+app.listen(3030, ()=>{
     console.log('Hello bhai log')
 })
--- Day10/models/userModel.js
+++ Day10/models/userModel.js
@@ -2,19 +2,21 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://0.0.0.0:27017/Day10')
 
+const postSchema = mongoose.Schema({
+    
+        content: String,
+        date: {
+            type: Date,
+            default: Date.now()
+        }
+    }
+)
+
 const userSchema = mongoose.Schema({
     name: String,
     email: String,
     password: String,
-    post:[
-        {
-            content: String,
-            date: {
-                type: Date,
-                default: Date.now()
-            }
-        }
-    ]
-})
+    post:[postSchema]
+}) 
 
-mongoose.exports = mongoose.model('user', userSchema)
+module.exports = mongoose.model('user', userSchema)
