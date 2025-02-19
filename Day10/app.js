322be99f678e6a044c4b07f8b4832b7b5b87a6cf
not-developer-exe <official.additya.singh@gmail.com>
2025-02-19 11:28:56 +0530

    Day 12 initialised

--- Day10/app.js
+++ Day10/app.js
@@ -1,34 +1,54 @@
-const express = require('express')
-const app = express()
-const userModel = require('./models/userModel')
+const express = require("express");
+const app = express();
+const userModel = require("./models/userModel");
+const postModel = require("./models/postModel")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+//// Creation of User
+app.post("/create", async (req, res) => {
+  let user = await userModel.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
 
-app.get('/', (req,res)=>{
-    res.send('Hey')
-})
+  console.log(user);
+  res.send(user);
+});
 
-app.post('/create',async (req, res)=>{
-    let user = await userModel.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
 
-    console.log(user)
-    res.send(user)
-})
-
-app.post('/:name/post', async (req,res)=>{
+/// Referencing Relationship..
+app.post('/:name/create/post',async (req, res)=>{
     let user = await userModel.findOne({name: req.params.name})
-    user.post.push({content: 'Ye gyi post teeeesraaaaa...'})
+    let post = await postModel.create({
+       content: "vsjrjvbrhb",
+       user: user._id
+    })
+     
+    user.post.push(post._id)
     await user.save()
-    res.send(user)
+
+    res.send({user, post})
+
+})
+
+///Population..
+app.get('/posts', async (req,res)=>{
+    let post = await postModel.find().populate('user')
+
+    res.send(post)
+})
+
+app.get('/users', async (req,res)=>{
+    let users = await userModel.find().populate('post')
+
+    res.send(users)
 })
 
 
-app.listen(3030, ()=>{
-    console.log('Hello bhai log')
-})
+app.listen(3030, () => {
+  console.log("Hello bhai log");
+});
--- Day10/models/userModel.js
+++ Day10/models/userModel.js
@@ -2,21 +2,14 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://0.0.0.0:27017/Day10')
 
-const postSchema = mongoose.Schema({
-    
-        content: String,
-        date: {
-            type: Date,
-            default: Date.now()
-        }
-    }
-)
-
 const userSchema = mongoose.Schema({
     name: String,
     email: String,
     password: String,
-    post:[postSchema]
+    post: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post" ////// Whatever id will come, it'll get searched in Post Collection
+    }]
 }) 
 
 module.exports = mongoose.model('user', userSchema)
